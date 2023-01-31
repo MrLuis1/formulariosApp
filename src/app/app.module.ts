bc1ae9e80fb0bb0c1467fbb2dc4f764d87fb0015
Luis Perdomo <programmerlgp@gmail.com>
2023-01-31 16:27:33 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
+// !Modulos
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+
+// !Componentes
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { MenuItem } from '../interface/MenuItem.interface'
+
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.css']
+})
+export class SidemenuComponent {
+
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches'
+    }
+  ]
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches'
+    }
+  ]
+
+}
