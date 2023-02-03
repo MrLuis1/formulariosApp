1a536833bb95129504c1d262658139d39f964381
Luis Perdomo <programmerlgp@gmail.com>
2023-02-03 13:22:41 -0400

    second commit

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('form') form!: any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  precioValido(): boolean {
+    return this.form?.controls.precio?.touched && this.form?.form.controls.precio?.value < 0 
+  }
+
+  productoValido() {
+    return !this.form?.valid && this.form?.touched
+  }
+
+  guardar(data: NgForm) {
+    console.log(this.form)
   }
 
 }
